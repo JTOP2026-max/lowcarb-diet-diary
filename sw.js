2515e6feaec52199e1fb7f92fdc7f1580899f774
JTOP2026-max <safecar7249@gmail.com>
2026-08-20 18:42:18 +0800

    Bump PWA cache for V12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lowcarb-v10-shell';
+const CACHE='lowcarb-v12-shell';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
